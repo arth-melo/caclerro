818b98201ca2356cc514f3669af73b0f8582b7ac
Erysson Roberto <erysulrobert56@gmail.com>
2026-08-20 23:22:58 -0300

    Adiciona calculadora

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a + b  # ERRO: deveria multiplicar
+
+def divisao(a, b):
+    return a / b
+
+
+print("Soma:", soma(10, 5))
+print("Subtração:", subtracao(10, 5))
+print("Multiplicação:", multiplicacao(10, 5))
+print("Divisão:", divisao(10, 5))
